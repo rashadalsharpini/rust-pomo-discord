019512b89105083f4e64f206bfcf296981424f2e
Hexbyte <islamamara062@gmail.com>
2026-05-16 12:44:45 +0000

    implement a mute button by rashadalsharpini

--- install.sh
+++ install.sh
@@ -8,19 +8,19 @@ LATEST_TAG=$(curl -s "https://api.github.com/repos/$REPO/releases/latest" | grep
 
 echo "📦 Installing Pomodoro TUI ($LATEST_TAG) for $OS..."
 
-if [ -d "/usr/local/bin/pomo" ]; then
-    sudo rm -rf /usr/local/bin/pomo
+if [ -f "/usr/local/bin/pomo" ]; then
+  sudo rm -rf /usr/local/bin/pomo
 fi
 
 if [ "$OS" = "Linux" ]; then
-    URL="https://github.com/$REPO/releases/download/$LATEST_TAG/pomo-linux.tar.gz"
-    FILE="pomo-linux.tar.gz"
+  URL="https://github.com/$REPO/releases/download/$LATEST_TAG/pomo-linux.tar.gz"
+  FILE="pomo-linux.tar.gz"
 elif [ "$OS" = "Darwin" ]; then
-    URL="https://github.com/$REPO/releases/download/$LATEST_TAG/pomo-macos.tar.gz"
-    FILE="pomo-macos.tar.gz"
+  URL="https://github.com/$REPO/releases/download/$LATEST_TAG/pomo-macos.tar.gz"
+  FILE="pomo-macos.tar.gz"
 else
-    echo "❌ OS not supported."
-    exit 1
+  echo "❌ OS not supported."
+  exit 1
 fi
 
 curl -L $URL -o $FILE
@@ -29,8 +29,8 @@ tar -xzf $FILE
 BIN_NAME=$(ls | grep -E 'pomodoro-tui-discord|rust-pomo-discord' | head -n 1)
 
 if [ -z "$BIN_NAME" ]; then
-    echo "❌ Error: Could not find the binary file after extraction."
-    exit 1
+  echo "❌ Error: Could not find the binary file after extraction."
+  exit 1
 fi
 
 sudo mv "$BIN_NAME" /usr/local/bin/pomo
